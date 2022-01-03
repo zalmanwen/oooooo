e929c61ee0f7e72ffc3f1bf680fc64dc7eb82cc4
zalmanwen <zalmanwen@163.com>
2022-01-03 20:10:54 +0800

    Update MTSPCrack.js

--- xxx/xxxx/MTSPCrack.js
+++ xxx/xxxx/MTSPCrack.js
@@ -1,31 +1,3 @@
-/*
- *
- *
-脚本功能：蜜桃视频解锁无限次数
-软件版本：1.0.1
-下载地址：https://shrtm.nu/mt96
-脚本作者：Hausd0rff
-更新时间：2021.11.17
-电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
-
-********************************
-
-[rewrite_local]
-
-# 蜜桃视频解锁无限次数
-\/t\w{4}\/\d\/t\/ url 307 /token/aa1dceb9b04740e2c09623342303c6ef/t/
-^https?:\/\/.*\.youbangjiazheng\.com\/api\/Video\/m3u8\/id.+ url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/MTSPCrack.js
-
-[mitm] 
-
-hostname = *.youbangjiazheng.com
-*
-*
-*/
-
 
 var hausd0rff = $request.headers;
 
